602c704156e7226a9511688ce44164850f2c2996
kni-neu <k.ni@northeastern.edu>
2024-05-25 14:14:22 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Streamlit LLM Chatbot with RAG (Llama 3)
+
+This is a Streamlit LLM Application from Ollama, interfacing with Langchain, but with a Vector Store from ChromaDB.
+
+## References
+
+* [RAG with PDF upload](https://github.com/vndee/local-rag-example)
+* [Langchain obtaining embeddings](https://python.langchain.com/v0.1/docs/integrations/text_embedding/ollama/)
+* [Ollama blog with embeddings](https://ollama.com/blog/embedding-models)
+
---
+++ app.py
@@ -0,0 +1,34 @@
+from langchain_community.llms import Ollama
+import streamlit as st
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+
+# llm = Ollama(model="phi:latest", base_url="http://ollama-container:11434", verbose=True)
+llm = Ollama(model="llama3", base_url="http://ollama-container:11434", verbose=True)
+
+def sendPrompt(prompt):
+    global llm
+    response = llm.invoke(prompt)
+    return response
+
+st.set_page_config(page_title='My Little RAG', layout = 'wide', page_icon = 'https://media.licdn.com/dms/image/C5603AQFFBPEfIzXcgg/profile-displayphoto-shrink_200_200/0/1604552403158?e=2147483647&v=beta&t=mWcPkWfIc4XZ17UBffkKw8Q9D9P0RweCfhExkb_JQAM', initial_sidebar_state = 'auto')
+st.title("Karl's Little Chatbot with a Database")
+if "messages" not in st.session_state.keys(): 
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Ask me a question !"}
+    ]
+
+if prompt := st.chat_input("Your question"): 
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+for message in st.session_state.messages: 
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+        
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = sendPrompt(prompt)
+            print(response)
+            st.write(response)
+            message = {"role": "assistant", "content": response}
+            st.session_state.messages.append(message) 
